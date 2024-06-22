d6401d8c130111c2cfc7a09d33ad26d4249c65cf
Fiarr4ik <fiarr4ik@gmail.com>
2024-06-23 00:02:15 +0700

    feat: Сделал аннотацию по гайду

---
+++ spring-boot-starter-audit-lib/src/main/java/ru/fiarr4ik/springbootstarterauditlib/ExampleAspect.java
@@ -0,0 +1,24 @@
+package ru.fiarr4ik.springbootstarterauditlib;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExampleAspect {
+
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+
+        System.out.println(joinPoint.getSignature() + " выполнился за " + executionTime + "ms");
+        return proceed;
+    }
+
+}
