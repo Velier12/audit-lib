a939c6bf9fdf45dea6410b558ca7329d1d631bdd
Fiarr4ik <fiarr4ik@gmail.com>
2024-06-22 23:00:57 +0700

    init commit

---
+++ spring-boot-starter-audit-lib/src/test/java/ru/fiarr4ik/springbootstarterauditlib/SpringBootStarterAuditLibApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ik.springbootstarterauditlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterAuditLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/java/ru/fiarr4ik/auditlibhomework/AuditLibHomeworkApplication.java
@@ -0,0 +1,14 @@
+package ru.fiarr4ik.auditlibhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class AuditLibHomeworkApplication {
+        private AuditLibHomeworkApplication() {}
+
+        public static void main(String[] args) {
+             SpringApplication.run(AuditLibHomeworkApplication.class, args);
+        }
+
+    }
